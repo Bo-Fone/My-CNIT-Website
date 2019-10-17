2ad6878bfc6735b9227ec6e45338d81975c8ec87
Flinner <12705673+Bo-Fone@users.noreply.github.com>
2019-10-17 01:27:06 -0700

    Fixed hw4

--- cnit-133a/hw4/weather_app/index.js
+++ cnit-133a/hw4/weather_app/index.js
@@ -3,14 +3,14 @@ const argv = require('yargs').argv;
 
 let apiKey = '2ab86d863fb78dddb0bdee6277af6dc0';
 let city = argv.c || 'San Francisco';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 request(url, function (err, response, body) {
     if (err) {
         console.log('error:', error);
     } else {
         let report = JSON.parse(body);
-        let msg = `It is ${report.main.temp} Celsius in ${report.name}. The min temperature is ${report.main.temp_min} degrees, and the max temperature is ${report.main.temp_max} degrees.`;
+        let msg = `It is ${report.main.temp} Fahrenheit in ${report.name}. The min temperature is ${report.main.temp_min} degrees, and the max temperature is ${report.main.temp_max} degrees.`;
         console.log(msg);
     }
 });
